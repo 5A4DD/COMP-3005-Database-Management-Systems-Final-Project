ba43fbeba28216ee135dbd2d60fbf22669171ac8
Jace222 <jasonscs222@gmail.com>
2024-03-21 11:41:03 -0400

    Local Database connection
    connecting to database (only local right now)
    
    npm install pg command

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # COMP3005_PROJECT
+
+To run the server
+    1. npm install pg
+
+
+To connect to the database as a user (to edit)
+    1.  Name:       FromTheTop
+        Password:  MAKEITDROP
--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: '',
-    password: '',
+    database: 'FitnessClub',
+    password: 'postgres',
     port: 5432,
 });
 
+async function getMembers() {
+    try {
+        const res = await pool.query('SELECT * FROM Member'); // Example query
+        console.log(res.rows);
+    } catch (err) {
+        console.error(err);
+    }
+}
 
+getMembers();
