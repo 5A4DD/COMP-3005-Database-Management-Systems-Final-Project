aad5d89cd7648be1a0d188feb4c976130035a5eb
Saad Rizwan <saadrizwan@cmail.carleton.ca>
2024-03-23 15:53:40 -0400

    new fix!

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'FitnessClub',
-    password: 'postgres',
+    user: 'ufqbm7pv6j7bi6',
+    host: 'c8lcd8bq1mia7p.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+    database: 'd82chc2ib2bi2g',
+    password: 'p8d5705ddc8edc70639d612f2ac3ed0efe86b049e6da4acec02245ab524f50bf3',
     port: 5432,
     ssl: {
         rejectUnauthorized: false,
