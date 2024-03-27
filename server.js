62d3b86f4617a9c14aa05d9787fcad6b83c3e0d5
Jace222 <jasonscs222@gmail.com>
2024-03-26 23:08:19 -0400

    all html pages connect to server
    hehehawrrr

--- server.js
+++ server.js
@@ -1,5 +1,43 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 
+// const pool = new Pool({
+//     user: 'ufqbm7pv6j7bi6',
+//     host: 'c8lcd8bq1mia7p.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+//     database: 'd82chc2ib2bi2g',
+//     password: 'p8d5705ddc8edc70639d612f2ac3ed0efe86b049e6da4acec02245ab524f50bf3',
+//     port: 5432,
+//     ssl: {
+//         rejectUnauthorized: false,
+//     }
+// });
+
+// // const pool = new Pool({          //local
+// //     user: 'postgres',
+// //     host: 'localhost',
+// //     database: 'FitnessClub',
+// //     password: 'postgres',
+// //     port: 5432,
+// // });
+
+// async function getMembers() {
+//     try {
+//         const res = await pool.query('SELECT * FROM Member'); // Example query
+//         console.log(res.rows);
+//     } catch (err) {
+//         console.error(err);
+//     }
+// }
+
+// getMembers();
+
+
+
+const express = require('express');
+const { Pool } = require('pg');
+const app = express();
+const port = 3000;
+
+// PostgreSQL pool setup
 const pool = new Pool({
     user: 'ufqbm7pv6j7bi6',
     host: 'c8lcd8bq1mia7p.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
@@ -11,21 +49,67 @@ const pool = new Pool({
     }
 });
 
-// const pool = new Pool({          //local
-//     user: 'postgres',
-//     host: 'localhost',
-//     database: 'FitnessClub',
-//     password: 'postgres',
-//     port: 5432,
-// });
+// Serve static files from 'views' folder
+app.use(express.static('views'));
+app.use('/styles', express.static('styles'));
 
-async function getMembers() {
+// Redirect root to Member's index.html
+app.get('/index.html', (req, res) => {
+    res.sendFile('index.html', { root: './views/Member' });
+});
+
+app.get('/billings.html', (req, res) => {
+    res.sendFile('billings.html', { root: './views/Admin' });
+});
+
+app.get('/booking.html', (req, res) => {
+    res.sendFile('booking.html', { root: './views/Admin' });
+});
+
+app.get('/equipment-maintenance.html', (req, res) => {
+    res.sendFile('equipment-maintenance.html', { root: './views/Admin' });
+});
+
+app.get('/about.html', (req, res) => {
+    res.sendFile('about.html', { root: './views/Member' });
+});
+
+app.get('/contact.html', (req, res) => {
+    res.sendFile('contact.html', { root: './views/Member' });
+});
+
+app.get('/login.html', (req, res) => {
+    res.sendFile('login.html', { root: './views/Member' });
+});
+
+app.get('/register.html', (req, res) => {
+    res.sendFile('register.html', { root: './views/Member' });
+});
+
+app.get('/set-availability.html', (req, res) => {
+    res.sendFile('set-availability.html', { root: './views/Trainer' });
+});
+
+app.get('/view-member.html', (req, res) => {
+    res.sendFile('view-member.html', { root: './views/Trainer' });
+});
+
+app.get('/view-schedule.html', (req, res) => {
+    res.sendFile('view-schedule.html', { root: './views/Trainer' });
+});
+
+// API endpoint to get members
+app.get('/api/members', async (req, res) => {
     try {
-        const res = await pool.query('SELECT * FROM Member'); // Example query
-        console.log(res.rows);
+        const dbRes = await pool.query('SELECT * FROM Member');
+        res.json(dbRes.rows);
     } catch (err) {
         console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
     }
-}
+});
 
-getMembers();
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/index.html`);
+});
